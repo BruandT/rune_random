f9795ded73a15752b8a5e66160ceade8371cbdff
Thomas Bruand <thomas.bruand@gmail.com>
2024-01-06 14:26:22 +0100

    Ajout de div pour le placement du contenu de la page Home

--- src/layouts/Default/Default.jsx
+++ src/layouts/Default/Default.jsx
@@ -1,14 +1,20 @@
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import { Outlet } from "react-router-dom";
+import Navigation from "../../components/Navigation/Navigation";
 
 function Default() {
   return (
     <>
-     <div className="min-h-screen flex flex-col">
-      <Header />
-      <Outlet />
-      <Footer />
+      <div className='flex'>
+        <div>
+          <Navigation />
+        </div>
+        <div className='flex flex-grow flex-col'>
+          <Header />
+          <Outlet />
+          <Footer />
+        </div>
       </div>
     </>
   );
