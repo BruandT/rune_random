65c8e96fe1a47def49be65a377af3440c12a82cc
Thomas Bruand <thomas.bruand@gmail.com>
2024-01-06 14:24:51 +0100

    Ajout du contenu et mise en style du compenent

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
-import './Header.css'
+import React from "react";
+import "./Header.css";
 
 function Header() {
-    return (
-        <>
-        <header className="h-36 bg-skin-bg-primary text-skin-base">
-        <h1>Je suis un HEADER !</h1>
-        </header>
-        </>
-    );
+  return (
+    <>
+      <header className='h-36 bg-skin-bg-primary '>
+        <div className="mt-5 ml-5 flex">
+        <img className='h-16' src="assets/logo_site.png" alt="logo du site"/>
+        <h1 className=" w-48 text-skin-color text-2xl">Summoners War Randomizer</h1>
+        </div>
+      </header>
+    </>
+  );
 }
 
 export default Header;
